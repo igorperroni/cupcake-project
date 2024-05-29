64af266cc00974220b88c31a5f8602c9fc5b4984
Lucas Gontijo <lucas.gon.t@hotmail.com>
2024-05-29 20:58:41 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,9 @@ import App from './App.jsx'
 import './styles.css'
 
 // Your Parse initialization configuration goes here
-const PARSE_APPLICATION_ID = '6sF4hy2SEQZDWkcXvrbLmwGA4IKImAcsljOaoiUw'
+const PARSE_APPLICATION_ID = 'MspIRJCe9DCaov9UL3kUexAa7g3yCKYnBvOrD6D0'
 const PARSE_HOST_URL = 'https://parseapi.back4app.com/'
-const PARSE_JAVASCRIPT_KEY = 'NyIecreI5iZe3DCDp9bC1mNFVOO4tWBMQFmSbS8o'
+const PARSE_JAVASCRIPT_KEY = 'Rn6Og31xstfgmI7x5JmA5B3bYmfrZzobUUrS8402'
 
 Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY)
 Parse.serverURL = PARSE_HOST_URL
